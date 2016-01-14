9b575991f39fdb0b33119ee00e0d7b8fda67853c
Duane Ulibarri <dulibarri10@cnm.edu>
2016-01-14 16:22:58 -0700

    Adjusted some indentation and added some CSS

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
 	<head>
-			<meta charset="utf-8">
-				<link rel="stylesheet" href="css/master.css" type="text/css"/>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/master.css" type="text/css"/>
 		<title>Just Me!</title>
 	</head>
-			<body>
-				<div class="bg-blue border-black" id="paragraph-1">
-					<h1>Rockabilly Guitar</h1>
-				<p>I'm going to write a little bit about me, mainly my hobby and passion, music!! I play bluegrass guitar, dobro, pedal steel guitar
-					and the mandolin. I'm also a Fender Telecaster Chickin' Picker! The style of music that I play is bluegrass, country and
-					rockabilly in the style of Scotty Moore. Scotty Moore was Elvis Presley's guitar player at his Sun Session Studios where he recorded,
-					"That's All Right Mama".</p
-					</div>
-				<div class="bg-red border-black" id="paragraph-2">
+	<body>
+		<div class="bg-blue border-black" id="paragraph-1">
+			<h1>Rockabilly Guitar</h1>
+			<p>I'm going to write a little bit about me, mainly my hobby and passion, music!! I play bluegrass guitar, dobro, pedal steel guitar
+				and the mandolin. I'm also a Fender Telecaster Chickin' Picker! The style of music that I play is bluegrass, country and
+				rockabilly in the style of Scotty Moore. Scotty Moore was Elvis Presley's guitar player at his Sun Session Studios where he recorded,
+				"That's All Right Mama".</p>
+		</div>
+		<div class="bg-red border-black" id="paragraph-2">
 
-					<h2>Influences</h2>
-					<p>My influences in the acoustic guitar are Doc Watson and Clarance White. They were both
-					bluegrass pickers. In the electric guitar my influences are many. They include Paul Burilson, Don Rich, Cliff Gallup and
-					Danny Gatton. What they all have in common is the guitar they play, the Fender Telecaster guitar. The teleaster is
-					a versatile guitar that can be played in many styles including country, jazz, shredding and of course chicken pickin'.
-					The tele has a very bright and punchy tone that is very distinct. The tele can be heard in countless country and jazz records.
-					I was five years old when I heard Buck Owens guitarist chicken pick for the first time and I was hooked.
-				</p>
-					</div>
-					</body>
+			<h2>Influences</h2>
+			<p>My influences in the acoustic guitar are Doc Watson and Clarance White. They were both
+			bluegrass pickers. In the electric guitar my influences are many. They include Paul Burilson, Don Rich, Cliff Gallup and
+			Danny Gatton. What they all have in common is the guitar they play, the Fender Telecaster guitar. The teleaster is
+			a versatile guitar that can be played in many styles including country, jazz, shredding and of course chicken pickin'.
+			The tele has a very bright and punchy tone that is very distinct. The tele can be heard in countless country and jazz records.
+			I was five years old when I heard Buck Owens guitarist chicken pick for the first time and I was hooked.
+		</p>
+			</div>
+			</body>
 </html>
 
 
